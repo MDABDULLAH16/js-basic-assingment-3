ce88e2380e01b14df4b6bd1f1842c7560726f985
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-08-04 15:23:29 +0600

    problem-6 solved

---
+++ problem-6.js
@@ -0,0 +1,10 @@
+/** Problem 06 :  (Current Salary )  */
+var experience = 3;
+var startingSalary = 15000;
+//write your code here
+var salary = startingSalary;
+
+for (var i = 1; i <= experience; i++) {
+  salary = salary + salary * 0.05;
+}
+console.log(parseFloat(salary.toFixed(2)));
